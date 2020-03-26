c57b33e8117e3071bb45d2fe3dfd05f15b3fe0f0
Livoras Dai <me@livoras.com>
2020-03-26 15:03:50 +0800

    feat: parse if else if and else

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import { parse, DefaultTreeDocument, DefaultTreeTextNode, DefaultTreeElement, DefaultTreeNode, Attribute } from "parse5"
 import { TAGS_MAP } from "./constants"
 
+interface TreeNode extends DefaultTreeElement {
+  attrsMap: Map<string, Attribute>,
+  isIf: boolean,
+  isElseIf: boolean,
+  isElse: boolean,
+  nextElseTemplateId: number,
+  templateId: number,
+}
+
+const ELSE_BLOCK_TEMPLSTE_PREFIX = "elseBlock"
+
 const html = `
 <view wx:for="{{list}}">
   <!-- 注释 -->
@@ -14,79 +25,119 @@ const html = `
     else Data
   </view>
   <input autofocus />
+  <view wx:if="{{item.name === 'a'}}"> aaa </view>
 </view>
 `
 
-export const wxml2angular = (wxmlString: string): string => {
+interface IContext {
+  lines: string[],
+  ngTemplateCounter: number,
 }
 
 export const wxml2angular = (wxmlString: string): string => {
   const ast = parse(wxmlString) as any
-  const fragment: DefaultTreeElement[] = ast.childNodes[0].childNodes[1].childNodes
-  const angularTemplateLines = []
-  wxmlFragment2angular(fragment, angularTemplateLines)
-  return angularTemplateLines.join("")
+  const ctx = { lines: [], ngTemplateCounter: 0 }
+  const fragment: TreeNode[] = preprocessNodes(ast.childNodes[0].childNodes[1].childNodes, ctx)
+  wxmlFragment2angular(fragment, ctx)
+  return ctx.lines.join("")
 }
 
-export const wxmlFragment2angular = (fragment: DefaultTreeElement[], lines: string[]) => {
+const preprocessNodes = (tree: TreeNode[], ctx: IContext): TreeNode[] => {
+  let prev: null | TreeNode
+  for (const node of tree) {
+    node.attrsMap = new Map()
+    if (node.attrs) {
+      for (const attr of node.attrs) {
+        node.attrsMap.set(attr.name, attr)
+      }
+      if (node.attrsMap.get("wx:if")) {
+        node.isIf = true
+      } else if (node.attrsMap.get("wx:elif")){
+        node.isElseIf = true
+      } else if (node.attrsMap.get("wx:else")) {
+        node.isElse = true
+      }
+    }
+    if (node.childNodes) {
+      preprocessNodes(node.childNodes as TreeNode[], ctx)
+    }
+    if ((node.isElse || node.isElseIf) && prev) {
+      node.templateId = ++ctx.ngTemplateCounter
+      prev.nextElseTemplateId = node.templateId
+    }
+    if (isNormalNode(node)) {
+      prev = node
+    }
+  }
+  return tree
+}
+
+export const wxmlFragment2angular = (fragment: TreeNode[], ctx: IContext) => {
   for (const node of fragment) {
     if (!node.nodeName.startsWith("#")) {
-      lines.push(parseWxmlNodeToAngularStartTag(node))
-      wxmlFragment2angular(node.childNodes as any, lines)
-      lines.push(getParsedWxmlEndTagName(node))
+      ctx.lines.push(parseWxmlNodeToAngularStartTag(node))
+      wxmlFragment2angular(node.childNodes as any, ctx)
+      ctx.lines.push(getParsedWxmlEndTagName(node))
     } else {
       if (node.nodeName === "#text") {
-        lines.push((node as any).value)
+        ctx.lines.push((node as any).value)
       } else {
-        lines.push(`<!--${(node as any).data}-->`)
+        ctx.lines.push(`<!--${(node as any).data}-->`)
       }
     }
   }
 }
 
-const parseWxmlNodeToAngularStartTag = (node: DefaultTreeElement): string => {
+const parseWxmlNodeToAngularStartTag = (node: TreeNode): string => {
   const tagName = getTagNameByWxmlNode(node)
   const attrsStr = parseWxmlNodeToAttrsString(node)
-  return `<${tagName} ${attrsStr}>`
+  return `${isElseOrIfElseNode(node) ? `<ng-template #${ELSE_BLOCK_TEMPLSTE_PREFIX}${node.templateId}${node.isElseIf ? ` *ngIf="${getIfElseAttr(node)}"` : ''}>` : ''}<${tagName} ${attrsStr}>`
 }
 
-const getParsedWxmlEndTagName = (node: DefaultTreeElement): string => {
-  return `</${getTagNameByWxmlNode(node)}>`
+const getIfElseAttr = (node: TreeNode): string => {
+  return getNgIfElseAttrValue(node.attrsMap.get("wx:elif"), node)
 }
 
-const getTagNameByWxmlNode = (node: DefaultTreeElement) => {
+const getParsedWxmlEndTagName = (node: TreeNode): string => {
+  return `</${getTagNameByWxmlNode(node)}>${isElseOrIfElseNode(node) ? "</ng-template>" : ""}`
+}
+
+const getTagNameByWxmlNode = (node: TreeNode) => {
   return TAGS_MAP[node.nodeName] || node.nodeName
 }
 
-const parseWxmlNodeToAttrsString = (node: DefaultTreeElement): string => {
+const parseWxmlNodeToAttrsString = (node: TreeNode): string => {
   const attrsList = []
-  const attrsMap = parseWxmlAttrsToAttrsMap(node)
   for (const attr of node.attrs) {
-    attrsList.push(parseWxmlAttrToAngularAttrStr(attr, attrsMap))
+    attrsList.push(parseWxmlAttrToAngularAttrStr(attr, node))
   }
   return attrsList.join(" ")
 }
 
-const parseWxmlAttrsToAttrsMap = (node: DefaultTreeElement): Map<string, Attribute> => {
-  const map = new Map()
-  for (const attr of node.attrs) {
-    map[attr.name] = attr
-  }
-  return map
-}
-
-const parseWxmlAttrToAngularAttrStr = (attr: Attribute, attrsMap: Map<string, Attribute>): string => {
+const parseWxmlAttrToAngularAttrStr = (attr: Attribute, node: TreeNode): string => {
   const n = attr.name
   if (n === "wx:for") {
     return `*ngFor="let item of ${stripDelimiters(attr.value)}"`
   } else if (n === "wx:if") {
-
+    return `*ngIf="${getNgIfElseAttrValue(attr, node)}"`
   }
   return attr.value ? `${attr.name}=${attr.value}` : attr.name
 } 
 
+const getNgIfElseAttrValue = (attr: Attribute, node: TreeNode): string => {
+  return `${stripDelimiters(attr.value)}${node.nextElseTemplateId ? `; else elseBlock${node.nextElseTemplateId}` : ''}`
+}
+
 const stripDelimiters = (val: string): string => {
   return val.replace(/(^\{\{)|(\}\}$)/g, '')
+}
+
+const isNormalNode = (node: TreeNode): boolean => {
+  return !node.nodeName.startsWith("#")
+}
+
+const isElseOrIfElseNode = (node: TreeNode): boolean => {
+  return node.isElse || node.isElseIf
 }
 
 console.log(wxml2angular(html))
